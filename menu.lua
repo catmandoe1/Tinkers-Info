460350e5b1a2d92e763e5ea6c14efdd29bf18df0
catmandoe1 <73958516+catmandoe1@users.noreply.github.com>
2021-11-02 19:59:26 +0000

    Added "isConnected"
    Adds "Not connected"  to the ends of the a,b,c selection if the required component is not available and will not allow use of that specific function

--- menu.lua
+++ menu.lua
@@ -1,23 +1,38 @@
 --menu program
 local component = require("component")
+local isSmelteryConnected = component.isAvailable("smeltery")
+local isFurnaceConnected = component.isAvailable("seared_furnace")
+local isTankConnected = component.isAvailable("tinker_tank")
 smelteryInfo = require("smeltery")
 furnaceInfo = require("seared_furnace")
 tankInfo = require("tinker_tank")
 local active = true
 
+function isConnected(connected)
+	string = ""
+	if connected == false then
+		string = " - Not connected"
+	end
+	return string
+end
+
+smelteryNotConnected = isConnected(isSmelteryConnected)
+furnaceNotConnected = isConnected(isFurnaceConnected)
+tankNotConnected = isConnected(isTankConnected)
+
 while active do
-  print("---")
-  print("a = Smeltery")
-  print("b = Furnace")
-  print("c = Tank")
-  print("d = Exit program")
+	print("---")
+	print("a = Smeltery" .. smelteryNotConnected)
+	print("b = Furnace" .. furnaceNotConnected)
+	print("c = Tank" .. tankNotConnected)
+	print("d = Exit program")
   userIn = io.read()
-  if userIn == "a" or userIn == "A" then
-    smelteryInfo.main()
-  elseif userIn == "b" or userIn == "B" then
-    furnaceInfo.main()
-  elseif userIn == "c" or userIn == "C" then
-    tankInfo.main()
+	if userIn == "a" or userIn == "A" and isSmelteryConnected == true then
+		smelteryInfo.main()
+	elseif userIn == "b" or userIn == "B" and isFurnaceConnected == true then
+		furnaceInfo.main()
+	elseif userIn == "c" or userIn == "C" and isTankConnected == true then
+		tankInfo.main()
   elseif userIn == "d" or userIn == "D" then
     active = false
   else
